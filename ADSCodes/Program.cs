1e9407151795ff4feda74306aa71fc6c6ea8e205
Nadun Nimalarathna <nadun.n@azendtech.com>
2023-06-03 16:58:19 +0530

    String Validation

---
+++ ADSCodes/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ADSCodes;
+
+class Program
+{
+    //string validation
+    static Boolean IsUppercase(string s)
+    {
+        return s.All(char.IsUpper);
+    }
+
+    static Boolean IsPasswordComplex(string s) {
+        return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+    }
+
+
+    static void Main(string[] arg)
+    {
+        Console.WriteLine(IsPasswordComplex("Buddhi12"));
+    }
+}
+
