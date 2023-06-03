6969f76c10f1a8ba3e40b6cb3cebbd87b8f68199
Nadun Nimalarathna <nadun.n@azendtech.com>
2023-06-03 16:58:34 +0530

    String Validation

--- ADSCodes/Program.cs
+++ ADSCodes/Program.cs
@@ -4,20 +4,9 @@ namespace ADSCodes;
 
 class Program
 {
-    //string validation
-    static Boolean IsUppercase(string s)
-    {
-        return s.All(char.IsUpper);
-    }
-
-    static Boolean IsPasswordComplex(string s) {
-        return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
-    }
-
-
     static void Main(string[] arg)
     {
-        Console.WriteLine(IsPasswordComplex("Buddhi12"));
+        Console.WriteLine(StringValidate.IsPasswordComplex("Buddhi12"));
     }
 }
 
---
+++ ADSCodes/StringValidate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSCodes
+{
+    public static class StringValidate
+    {
+        //string validation
+        public static Boolean IsUppercase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+
+        public static Boolean IsPasswordComplex(string s)
+        {
+            return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+        }
+    }
+}
