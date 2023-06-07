937c87d2190e5adc3b1374a222012dbf5bc4f4b8
Nadun Nimalarathna <nadun.n@azendtech.com>
2023-06-07 21:26:21 +0530

    Fix

---
+++ ADSCodes/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace ADSCodes;
+
+class Node
+{
+   public string value { get; set; }
+   public Node left { get; set; }
+   public Node right { get; set; }
+    public Node(string val)
+    {
+        this.value = val;
+    }
+
+}
+
--- ADSCodes/Program.cs
+++ ADSCodes/Program.cs
@@ -39,16 +39,3 @@ class Program
         return outputStr;
     }
 }
-
-class Node
-{
-   public string value { get; set; }
-   public Node left { get; set; }
-   public Node right { get; set; }
-    public Node(string val)
-    {
-        this.value = val;
-    }
-
-}
-
